099e695ae0c35c82d6d8b90ac7f08c4b6219d0a6
rakeshraky <69849377+rakeshraky@users.noreply.github.com>
2020-08-26 18:56:38 +0530

    Update settings.py

--- djangovueproject/settings.py
+++ djangovueproject/settings.py
@@ -78,7 +78,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -140,4 +139,3 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
